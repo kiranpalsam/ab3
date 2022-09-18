dbdcabcd01dc8cd8562bf96c1997433e141f8f5e
kiranpalsam <44536664+kiranpalsam@users.noreply.github.com>
2022-09-18 19:00:26 -0400

    Update movieDatabase.php

--- movies/includes/movieDatabase.php
+++ movies/includes/movieDatabase.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "admin";
+$servername = "ab3lampstack-mysqldatabase-5rsm43fqoi23.c7oxqnztd8tr.us-east-1.rds.amazonaws.com";
+$username = "ab3user";
+$password = "password123";
 $dbname = "moviedb";
 
 $conn = mysqli_connect($servername, $username, $password,$dbname );

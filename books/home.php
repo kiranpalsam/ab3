04053a402d7ec100ab33affd4997eb1f3cd5f08b
kiranpalsam <44536664+kiranpalsam@users.noreply.github.com>
2022-09-22 18:09:23 -0400

    Update home.php

--- books/home.php
+++ books/home.php
@@ -35,8 +35,8 @@
         </div>
         <ul class="nav navbar-nav">
             <li ><a href="../index.php">Home</a></li>
-            <li class="active"><a href="./home.php">Health Services</a></li>
-            <li ><a href="../movies/home.php">Public Services</a></li>
+            <li class="active"><a href="./home.php">Books</a></li>
+            <li><a href="https://demoab3.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=2t166qkdi1jl8a5sedr7s7gsrk&redirect_uri=https://demoab3.rohan.aws-info.net/movies/home.php">Movies</a></li>
         </ul>
     </div>
 </nav>
